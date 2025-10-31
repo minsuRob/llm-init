5ada28dd5e58c67354c9ab08e8b81d4c76197bdf
minsu.lee <minsu.rob@gmail.com>
2025-11-01 00:56:52 +0900

    fix: 텍스트 데이터 전처리 추가

---
+++ src/utils/clean_text.py
@@ -0,0 +1,18 @@
+import re
+
+def clean_text(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        book_text = file.read()
+
+    cleaned_text = re.sub(r'\n+', ' ', book_text) # 줄바꿈을 빈칸으로 변경
+    cleaned_text = re.sub(r'\s+', ' ', cleaned_text) # 여러 빈칸을 하나의 빈칸으로
+
+    print("cleaned_" + filename, len(cleaned_text), "characters") # 글자 수 출력
+
+    with open("cleaned_" + filename, 'w', encoding='utf-8') as file:
+        file.write(cleaned_text)
+
+filenames_list = ["norway_forest.txt"]
+
+for filename in filenames_list:
+    clean_text(filename)
